2da61b1fcfb19b0d30debdac4a16082563bbe244
earl-stephens <stephensearl@hotmail.com>
2024-11-19 19:01:58 -0700

    Initial commit

---
+++ src/main/java/config/ProjectConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ *
+ * @author earltstephens
+ */
+@Configuration
+public class ProjectConfig {
+    
+}
